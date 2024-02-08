32665a64e64d090c27f2835204f4dd620fe6b245
ygref <jason.wesley.hc@gmail.com>
2024-02-08 16:51:35 -0500

    map fix, same line, margins, flex center

--- src/PlayersCharactersIndex.jsx
+++ src/PlayersCharactersIndex.jsx
@@ -3,10 +3,13 @@ export function PlayersCharactersIndex(props) {
     <div>
       <h1>Player / Character</h1>
       {props.playerscharacters.map((playerscharacter) => (
-        <div key={playerscharacter.id}>
-          <h2>
-            Player: {playerscharacter.playername} Character: {playerscharacter.character_name}
-          </h2>
+        <div key={playerscharacter.id} style={{ display: "flex", alignItems: "center" }}>
+          <h2 style={{ marginRight: "8px" }}>Player:</h2>
+          <p style={{ marginRight: "16px" }}>{playerscharacter.playername}</p>
+          <h2 style={{ marginRight: "8px" }}>Character:</h2>
+          <p style={{ marginRight: "16px" }}>{playerscharacter.character_name}</p>
+          <h2 style={{ marginRight: "8px" }}>Role:</h2>
+          <p>{playerscharacter.character_role}</p>
         </div>
       ))}
     </div>
